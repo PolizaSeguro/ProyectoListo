b1a773eceef057a61def95a45fcbe3771f8ded60
PolizaSeguro <123pirlo901@gmail.com>
2024-12-25 18:08:39 -0500

    Update server.js

--- Backend/models/server.js
+++ Backend/models/server.js
@@ -1,4 +1,3 @@
-// Servidor de Express
 const express = require("express");
 const http = require("http");
 const socketio = require("socket.io");
@@ -10,7 +9,7 @@ const connectDB = require("../database");
 class Server {
   constructor() {
     this.app = express();
-    this.port = process.env.PORT;
+    this.port = process.env.PORT || 3000;
     this.database = connectDB();
     // Http server
     this.server = http.createServer(this.app);
@@ -23,19 +22,17 @@ class Server {
 
   middlewares() {
     // Desplegar el directorio público
-    this.app.use(express.static(path.resolve(__dirname, "../public")));
+    this.app.use(express.static(path.resolve(__dirname, "../frontend/dist")));
 
     // CORS
     this.app.use(cors());
 
     // Catch-all route to serve index.html for React Router
     this.app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, '../public', 'index.html'));
+      res.sendFile(path.resolve(__dirname, '../frontend/dist', 'index.html'));
     });
   }
 
-  // Esta configuración se puede tener aquí o como propieda de clase
-  // depende mucho de lo que necesites
   configurarSockets() {
     new Sockets(this.io);
   }
