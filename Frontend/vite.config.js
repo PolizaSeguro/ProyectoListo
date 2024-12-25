d072d22cae41b505f0d47f12eb259bc916d15f7a
PolizaSeguro <123pirlo901@gmail.com>
2024-12-25 18:38:45 -0500

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,16 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
   base: '/',
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://proyecto-listo.vercel.app/',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
+  build: {
+    outDir: 'dist', 
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom']
+        }
       }
     }
   }
-})
+});
