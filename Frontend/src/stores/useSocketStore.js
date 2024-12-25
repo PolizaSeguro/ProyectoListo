736849bfcbc8012e8fc0e9bd322ff57c605bbe91
PolizaSeguro <123pirlo901@gmail.com>
2024-12-25 17:14:15 -0500

    Update useSocketStore.js

--- Frontend/src/stores/useSocketStore.js
+++ Frontend/src/stores/useSocketStore.js
@@ -2,7 +2,7 @@ import { create } from 'zustand';
 import { io } from 'socket.io-client';
 
 export const useSocketStore = create((set) => ({
-    socket : io('https://proyecto-listo-2wd6.vercel.app/:3000',{
+    socket : io('https://proyecto-listo-m2sc.vercel.app/',{
         transports: ['websocket']
     })
 }));
