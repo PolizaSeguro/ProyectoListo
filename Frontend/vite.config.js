2227736f5bff68d0c52310b7552f582f726ffc2e
PolizaSeguro <123pirlo901@gmail.com>
2024-12-25 21:06:17 -0500

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -3,15 +3,4 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Cambiar si la app no está en la raíz del dominio
-  build: {
-    outDir: 'dist', // Asegúrate de que coincida con el `distDir` en vercel.json
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom']
-        }
-      }
-    }
-  }
 });
