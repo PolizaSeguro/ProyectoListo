e5ef7bb7867af47b994f3f73a2d66534561f38a7
PolizaSeguro <123pirlo901@gmail.com>
2024-12-27 14:48:16 -0500

    Cambio Final

--- Frontend/src/stores/useSocketStore.js
+++ Frontend/src/stores/useSocketStore.js
@@ -2,7 +2,7 @@ import { create } from 'zustand';
 import { io } from 'socket.io-client';
 
 export const useSocketStore = create((set) => ({
-    socket : io('https://backend-proyecto-five.vercel.app',{
+    socket : io('http://147.93.40.118:3000',{
         transports: ['websocket']
     })
 }));
