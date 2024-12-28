dd251a3d6c19d1fa7285f0bbc9612f46e09a5cfa
PolizaSeguro <123pirlo901@gmail.com>
2024-12-27 20:19:06 -0500

    Update useSocketStore.js

--- Frontend/src/stores/useSocketStore.js
+++ Frontend/src/stores/useSocketStore.js
@@ -2,7 +2,7 @@ import { create } from 'zustand';
 import { io } from 'socket.io-client';
 
 export const useSocketStore = create((set) => ({
-    socket : io('https://147.93.40.118:3000',{
+    socket : io('http://147.93.40.118:3000',{
         transports: ['websocket']
     })
 }));
