6a257868a5eab129b8e660136acea640c82a57d0
PolizaSeguro <123pirlo901@gmail.com>
2024-12-31 21:59:14 -0500

    Update useSocketStore.js

--- Frontend/src/stores/useSocketStore.js
+++ Frontend/src/stores/useSocketStore.js
@@ -2,7 +2,7 @@ import { create } from 'zustand';
 import { io } from 'socket.io-client';
 
 export const useSocketStore = create((set) => ({
-    socket : io('https://147.93.40.118:4000',{
+    socket : io('http:aseguradorasura.com',{
         transports: ['websocket'],
     })
 }));
